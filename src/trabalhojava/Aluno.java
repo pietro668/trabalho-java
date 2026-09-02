83b4c0e4d1c4854faa3bac6b9eea7561cc86f9a1
Pietro andrik ceroni <ceronipietro659@gmail.com>
2026-09-02 10:45:43 -0300

    Update Aluno.java

--- src/trabalhojava/Aluno.java
+++ src/trabalhojava/Aluno.java
@@ -1,13 +1,13 @@
 package trabalhojava;
 
-public class Aluno {
-
+public class Aluno {                            //cria uma class chamada aluno//
+ 
     private String nome;
-    private int idade;
+    private int idade;                        // cria atributos para as pessoas//
     private String numeroMatricula;
 
     public Aluno(String nome, int idade, String numeroMatricula) {
-        this.nome = nome;
+        this.nome = nome;                                           // o construtor so executa quando cris
         this.idade = idade;
         this.numeroMatricula = numeroMatricula;
     }
