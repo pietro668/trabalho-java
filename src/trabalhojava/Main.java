99b5b41e114995320c2d0e7dcb9564e455a36138
Pietro andrik ceroni <ceronipietro659@gmail.com>
2026-09-02 10:55:39 -0300

    Add comments to Main class actions
    Added comments to clarify the purpose of each action in the main method.

--- src/trabalhojava/Main.java
+++ src/trabalhojava/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Aluno aluno = new Aluno("João", 15, "2025001");
+        Aluno aluno = new Aluno("João", 15, "2025001");                //cria o aluno com esses atributos//
         professor professor = new professor("Carlos", "Matemática");
         Turma turma = new Turma("1º Ano A", "Sala 10");
         Disciplina disciplina = new Disciplina("Matemática");
@@ -12,12 +12,12 @@ public class Main {
         Boletim boletim = new Boletim(8.5);
         Escola escola = new Escola("Escola Estadual ABC");
 
-        escola.cadastrarAluno();
-        matricula.matricularAluno();
+        escola.cadastrarAluno();      //cadastra o aluno na escola//
+        matricula.matricularAluno(); // matricula ele//
         turma.adicionarAluno();
-        professor.ministrarAula();
-        aluno.estudar();
-        disciplina.registrarNota();
+        professor.ministrarAula();  // representa o professor dando aula//
+        aluno.estudar();        //resultará a acâo escolhida//
+        disciplina.registrarNota();  //registra notas//
         boletim.emitirBoletim();
     }
 }
