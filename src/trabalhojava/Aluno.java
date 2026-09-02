4584371f3b14f8fe59dfdd1b71896b3f50967dda
Pietro andrik ceroni <ceronipietro659@gmail.com>
2026-09-02 10:46:52 -0300

    Refactor comments and improve constructor clarity

--- src/trabalhojava/Aluno.java
+++ src/trabalhojava/Aluno.java
@@ -7,12 +7,12 @@ public class Aluno {                            //cria uma class chamada aluno//
     private String numeroMatricula;
 
     public Aluno(String nome, int idade, String numeroMatricula) {
-        this.nome = nome;                                           // o construtor so executa quando cris
+        this.nome = nome;                                           // o construtor so executa quando criamos um novo objeto chamado aluno//
         this.idade = idade;
         this.numeroMatricula = numeroMatricula;
     }
 
     public void estudar() {
-        System.out.println(nome + " está estudando.");
+        System.out.println(nome + " está estudando.");               // cria uma ação pra pessoa//
     }
 }
